55fe522171eb1eecac48ed43d0fb7c8485bfdbe2
Star <137379612@qq.com>
2015-03-09 19:54:41 +0800

    test

--- vendor/assets/javascripts/ueditor/editor_config.js
+++ vendor/assets/javascripts/ueditor/editor_config.js
@@ -31,7 +31,7 @@
 
         // 服务器统一请求接口路径
         , serverUrl: "/utils/upload"
-
+,imageActionName: "/utils/upload"
         //工具栏上的所有的功能按钮和下拉框，可以在new编辑器的实例时选择自己需要的从新定义
         
         , toolbars: [[ "undo", "redo", "forecolor", "bold", "italic", "underline", "backcolor",
