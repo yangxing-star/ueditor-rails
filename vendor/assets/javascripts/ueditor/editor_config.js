d8fa6496472b89300c99a5fed2a6b75a0f222270
Star <137379612@qq.com>
2015-03-10 11:34:23 +0800

    serverUrl update

--- vendor/assets/javascripts/ueditor/editor_config.js
+++ vendor/assets/javascripts/ueditor/editor_config.js
@@ -30,7 +30,7 @@
         UEDITOR_HOME_URL: URL
 
         // 服务器统一请求接口路径
-        , serverUrl: "/utils/ueditor_config"
+        , serverUrl: "/utils/ueditor_upload"
         //工具栏上的所有的功能按钮和下拉框，可以在new编辑器的实例时选择自己需要的从新定义
         
         , toolbars: [[ "undo", "redo", "forecolor", "bold", "italic", "underline", "backcolor",
